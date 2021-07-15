1883c44271e2f064e6ce91dee571dd31784e5346
Eddy PIKS-L <Zymeth@users.noreply.github.com>
2021-07-15 16:04:42 +0200

    v13
    v13

--- pages/index.vue
+++ pages/index.vue
@@ -30,27 +30,10 @@
         });
         var t1 = this.$gsap.timeline(), mySplitText = new SplitType(".title", {type:"words,chars"}), chars = mySplitText.words;
           t1.from(chars, {delay: 0.6, duration: 1, opacity:0, y:50, transformOrigin:"0% 50% 100",  ease:"back", stagger: 0.1}, "+=0");
-      },
-      scrollpage(){
-          var container = document.querySelector(".main-content");
-          var height = container.clientHeight;
-          document.body.style.height = height + "px";
-          this.$gsap.to(container, {
-            y: -(height - document.documentElement.clientHeight),
-            scrollTrigger: {
-              trigger: document.body,
-              start: "top top",
-              end: "bottom bottom",
-              scrub: 0
-            }
-          });
       }
     },
     mounted() {
       this.appear();
-      this.$nextTick(function () {
-        this.scrollpage();
-      })
     }
   }
 </script>
