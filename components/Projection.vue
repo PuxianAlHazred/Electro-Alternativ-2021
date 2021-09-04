a7092eddf4b04daf251a0ef5dac2e739be8b1d4f
Yoann PIKS-L <50371128+PuxianAlHazred@users.noreply.github.com>
2021-09-04 09:10:16 +0200

    Update Projection.vue

--- components/Projection.vue
+++ components/Projection.vue
@@ -1,11 +1,11 @@
 <template>
     <section>
-      <div class="lieux">
+      <div class="projections">
   
         <div class="container">
   
           <div class="wopper">
-            <div class="marquee marqueelieu">
+            <div class="marquee marqueeprojection">
               <p>
                 PROJECTION  - PROJECTION - PROJECTION - PROJECTION - PROJECTION - PROJECTION -
               </p>
@@ -16,14 +16,14 @@
           </div>
   
           <ul>
-            <li id="abattoires">
-                <div class="lieu-img">a
-                  <img srcset="/images/lieux/Abattoirs-xl.webp 1440w, /images/lieux/Abattoirs-md.webp 1024w, /images/lieux/Abattoirs-xs.webp 767w"
+            <li id="abattoires-projection">
+                <div class="projection-img">a
+                  <img srcset="/images/projectionx/Abattoirs-xl.webp 1440w, /images/projectionx/Abattoirs-md.webp 1024w, /images/projectionx/Abattoirs-xs.webp 767w"
                       sizes="(max-width: 1440px), (max-width: 1024px), (max-width: 767px)"
-                      src="/images/lieux/Abattoirs-xs.webp" loading="lazy" alt="Les Abattoires - Toulouse" />
+                      src="/images/projectionx/Abattoirs-xs.webp" loading="lazy" alt="Les Abattoires - Toulouse" />
                 </div>
-                <Panthere :typee="'lieu'" />
-                <div class="lieu-content">
+                <Panthere :typee="'projection'" />
+                <div class="projection-content">
                     <div class="center">
                         <h2>les abattoirs</h2>
                         <a href="https://www.google.com/maps/dir//les+abattoirs/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x12aebb6dfd01bc3b:0x1e32c9b07a10045f?sa=X&ved=2ahUKEwiUi8OekYjyAhXByIUKHQ0_D2kQ9RcwH3oECGEQBQ"  target="_blank">s'y rendre</a>
@@ -44,50 +44,50 @@
   
       },
       methods: {
-        animationLieux() {
+        animationprojections() {
           if( window.innerWidth > 1024) {
-              let leslieux = this.$gsap.timeline({
+              let lesprojections = this.$gsap.timeline({
                 scrollTrigger: {
-                  trigger: ".lieux .container",
+                  trigger: ".projections .container",
                   start: "top top+=450",
                   end: "top top+=50",
                   scrub: true
                 }
               })
-              .fromTo(".marqueelieu",
+              .fromTo(".marqueeprojection",
                 { "font-size": "100px", "line-height": "100px","color": "#95165d", ease: 'power2.inOut'},
                 { "font-size": "45px", "line-height": "75px","color": "#000", ease: 'power2.inOut'}
               )
           }
           if(window.innerWidth <= 1024) {
-              let leslieux = this.$gsap.timeline({
+              let lesprojections = this.$gsap.timeline({
                 scrollTrigger: {
-                  trigger: ".lieux .container",
+                  trigger: ".projections .container",
                   start: "top top+=450",
                   end: "top top+=50",
                   scrub: true
                 }
               })
-              .fromTo(".marqueelieu",
+              .fromTo(".marqueeprojection",
                 { "font-size": "75px", "line-height": "75px","color": "#95165d", ease: 'power2.inOut'},
                 { "font-size": "35px", "line-height": "75px","color": "#000", ease: 'power2.inOut'}
               )
           }
   
-          this.$gsap.utils.toArray(".lieu-content").forEach(e => {
+          this.$gsap.utils.toArray(".projection-content").forEach(e => {
   
-            var lieu = e.getElementsByTagName('h2');
+            var projection = e.getElementsByTagName('h2');
             var itineraire = e.getElementsByTagName('a');
             var mobile = e.parentNode;
   
-            var titleLieu = this.$gsap.timeline({paused:true}), SplitLieu = new SplitType(lieu , {type:"words"}), words = SplitLieu.words;
-                titleLieu.from(words, {delay: 0.2, duration: 0.5, opacity:0, y:-50, transformOrigin:"0% 50% 100",  ease:"power2.inOut", stagger: 0.1}, "+=0");
+            var titleprojection = this.$gsap.timeline({paused:true}), Splitprojection = new SplitType(projection , {type:"words"}), words = Splitprojection.words;
+                titleprojection.from(words, {delay: 0.2, duration: 0.5, opacity:0, y:-50, transformOrigin:"0% 50% 100",  ease:"power2.inOut", stagger: 0.1}, "+=0");
   
-            var itineraireLieu = this.$gsap.timeline({paused:true}), SplitItineraire = new SplitType(itineraire , {type:"chars"}), chars = SplitItineraire.chars;
-                itineraireLieu.from(chars, {delay: 0.2, duration: 0.5, opacity:0, y:100, transformOrigin:"0% 50% 100",  ease:"power2.inOut", stagger: 0}, "+=0");
+            var itineraireprojection = this.$gsap.timeline({paused:true}), SplitItineraire = new SplitType(itineraire , {type:"chars"}), chars = SplitItineraire.chars;
+                itineraireprojection.from(chars, {delay: 0.2, duration: 0.5, opacity:0, y:100, transformOrigin:"0% 50% 100",  ease:"power2.inOut", stagger: 0}, "+=0");
   
             if( window.innerWidth > 767) {
-                var blocLieu = this.$gsap.timeline({
+                var blocprojection = this.$gsap.timeline({
                   scrollTrigger: {
                         trigger: e,
                         start: "center+=75px bottom-=100px",
@@ -95,27 +95,27 @@
                         scrub: false,
                         toggleActions: "play reverse play reverse",
                         onEnterBack: e => {
-                          titleLieu.play();
-                          itineraireLieu.play();
+                          titleprojection.play();
+                          itineraireprojection.play();
                         },
                         onLeaveBack: e => {
-                          titleLieu.reverse();
-                          itineraireLieu.reverse();
+                          titleprojection.reverse();
+                          itineraireprojection.reverse();
                         },
                         onEnter: e => {
-                          titleLieu.play();
-                          itineraireLieu.play();
+                          titleprojection.play();
+                          itineraireprojection.play();
                         },
                         onLeave: e => {
-                          titleLieu.reverse();
-                          itineraireLieu.reverse();
+                          titleprojection.reverse();
+                          itineraireprojection.reverse();
                         },
                         //markers: {startColor: "pink", endColor: "pink", fontSize: "25px", fontWeight: "bold", indent: 0}
                   }
                 }).fromTo(e, {  y: -100, opacity: 0, ease: "linear"}, {  y: 0, opacity: 1, ease: "linear"});
               }
               if( window.innerWidth <= 767) {
-                var blocLieu = this.$gsap.timeline({
+                var blocprojection = this.$gsap.timeline({
                   scrollTrigger: {
                         trigger: mobile,
                         start: "top center+=100px",
@@ -123,27 +123,27 @@
                         scrub: false,
                         toggleActions: "play reverse play reverse",
                         onEnterBack: e => {
-                          titleLieu.play();
-                          itineraireLieu.play();
+                          titleprojection.play();
+                          itineraireprojection.play();
                         },
                         onLeaveBack: e => {
-                          titleLieu.reverse();
-                          itineraireLieu.reverse();
+                          titleprojection.reverse();
+                          itineraireprojection.reverse();
                         },
                         onEnter: e => {
-                          titleLieu.play();
-                          itineraireLieu.play();
+                          titleprojection.play();
+                          itineraireprojection.play();
                         },
                         onLeave: e => {
-                          titleLieu.reverse();
-                          itineraireLieu.reverse();
+                          titleprojection.reverse();
+                          itineraireprojection.reverse();
                         },
                         //markers: {startColor: "pink", endColor: "pink", fontSize: "25px", fontWeight: "bold", indent: 0}
                   }
                 }).fromTo(e, {  y: -100, opacity: 0, ease: "linear"}, {  y: 0, opacity: 1, ease: "linear"});
               }
           });
-          this.$gsap.utils.toArray(".lieu-img").forEach(e => {
+          this.$gsap.utils.toArray(".projection-img").forEach(e => {
   
             var img = e.getElementsByTagName('img');
             var mobile = e.parentNode;
@@ -176,7 +176,7 @@
             }
   
           });
-          this.$gsap.utils.toArray(".intro-panth.lieu").forEach(e => {
+          this.$gsap.utils.toArray(".intro-panth.projection").forEach(e => {
             var panthOpa = this.$gsap.from(e, {delay: 0.5, duration: 0.5, opacity:0, ease:"power2.inOut"});
             if( window.innerWidth > 767) {
                 var blocPanth = this.$gsap.timeline({
@@ -207,8 +207,8 @@
         },
       },
       mounted() {
-        this.animationLieux();
-        window.addEventListener('resize', this.animationLieux);
+        this.animationprojections();
+        window.addEventListener('resize', this.animationprojections);
       }
     }
   </script>
